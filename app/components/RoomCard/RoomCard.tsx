890b99f1a113d9bd8326429c14c94fb5854b4895
ThatNinjaGuy <vishalgautam.dtu@gmail.com>
2024-01-14 13:31:42 +0530

    Optimizing for CSR website

--- app/components/RoomCard/RoomCard.tsx
+++ app/components/RoomCard/RoomCard.tsx
@@ -9,10 +9,10 @@ type Props = {
 
 const RoomCard: FC<Props> = (props) => {
   const {
-    room: { coverImage, name, price, type, description, slug, isBooked },
+    room: { coverImage, name, type, description, slug, isBooked },
   } = props;
   return (
-    <div className="rounded-xl w-72 mb-10 mx-auto md:mx-0 overflow-hidden text-black">
+    <div className="flex flex-col rounded-xl w-72 mb-10 mx-auto md:mx-0 overflow-hidden text-black">
       <div className="h-60 overflow-hidden">
         <Image
           src={coverImage.url}
@@ -22,19 +22,24 @@ const RoomCard: FC<Props> = (props) => {
           className="img scale-animation"
         />
       </div>
-      <div className="p-4 bg-white">
-        <div className="flex justify-between text-xl font-semibold">
-          <p>{name}</p>
-          <p className="font-black text-tertiary-dark">₹ {price}</p>
+      <div className="flex flex-col justify-between flex-grow p-4 bg-white">
+        <div>
+          <div className="flex justify-between text-xl font-semibold">
+            <p>{name}</p>
+            {/* Price can be shown here if needed */}
+          </div>
+          <p className="pt-2 text-xs">{type} Room</p>
+          {/* Set a fixed number of lines for the description */}
+          {/* <p className="pt-3 pb-6 overflow-hidden text-xs line-clamp-N">
+            {description}
+          </p> */}
+          <p className="pt-3 pb-6">{description.slice(1, 100)}...</p>
         </div>
-        <p className="pt-2 text-xs">{type} Room</p>
-        <p className="pt-3 pb-6">{description.slice(1, 100)}...</p>
-
         <Link
           href={`/ideas/${slug.current}`}
-          className="bg-primary inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:translate-y-2 hover:shadow-lg transition-all duration-500"
+          className="bg-primary mt-4 inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:translate-y-2 hover:shadow-lg transition-all duration-500"
         >
-          {isBooked ? "BOOKED" : "BOOK NOW"}
+          {isBooked ? "Impact More" : "Shape Lives"}
         </Link>
       </div>
     </div>
